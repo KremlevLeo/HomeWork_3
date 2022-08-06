a15717a6fdae0ed92dae807ac34f500e8f6f8ae7
Leonid <leeonidix@gmail.com>
2022-08-06 17:08:39 +0500

    Done Task 002

--- Task_002/Program.cs
+++ Task_002/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+Console.Write("Enter number: ");
+string str = Console.ReadLine();
+int[] arrNumb = str.Select(x => int.Parse(x.ToString())).ToArray();
+int result = 0;
+for (int i = 0; i < arrNumb.Length; i++)
+{
+    result += arrNumb[i];
+}
+Console.WriteLine($"{str} -> {result}");
