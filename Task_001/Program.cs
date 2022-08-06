1f82e622da6116bc1fb2a00cf96139429fa5b9d2
Leonid <leeonidix@gmail.com>
2022-08-06 15:03:48 +0500

    Done Task 001

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+Console.Write("Enter firs numer: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Enter second numer: ");
+int b = int.Parse(Console.ReadLine());
+int result = 1;
+for (int i = 0; i < b; i++)
+{
+    result *=a;
+}
+Console.WriteLine($"{a}, {b} -> {result}");
