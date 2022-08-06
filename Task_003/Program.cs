438f043cee857d55ee3f4138af2992d0907dc319
Leonid <leeonidix@gmail.com>
2022-08-06 18:03:05 +0500

    Done Task 003

--- Task_003/Program.cs
+++ Task_003/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+int[] arrNumb = new int[8];
+Random rnd = new Random();
+for (int i = 0; i < 8; i++)
+{
+    arrNumb[i] = rnd.Next(0, 101);
+    Console.Write($"{arrNumb[i]} ");
+}
+
